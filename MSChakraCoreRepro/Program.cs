9096aeafad489dd61320fe2ac2029ed77b65b3b5
Meenakshi Sundaram V <sundaram.viswanathan@ecyber.com>
2019-04-23 16:08:59 +0530

    Add project files.

---
+++ MSChakraCoreRepro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSChakraCoreRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
